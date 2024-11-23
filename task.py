c010bdd4bb2780fd2242f28b056655bd4aef7b53
alphadekw <163714728+alphadekw@users.noreply.github.com>
2024-11-23 17:18:57 +0300

    Add files via upload

---
+++ task.py
@@ -0,0 +1,21 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+
+grades_0 = sum(grades [0]) / len(grades [0])
+grades_1 = sum(grades [1]) / len(grades [1])
+grades_2 = sum(grades [2]) / len(grades [2])
+grades_3 = float(int(sum(grades [3]) / len(grades [3])))
+grades_4 = sum(grades [4]) / len(grades [4])
+students = {}
+students['Johnny'] = grades_0
+students['Bilbo'] = grades_1
+students['Steve'] = grades_2
+students['Khendrik'] = grades_3
+students['Aaron'] = grades_4
+
+print(students)
+
+
+
+
+
